de3bca3761041be860be0d3e21a14ef193c5ffe8
Walwarsss <57537471+Walwarsss@users.noreply.github.com>
2022-10-19 15:36:20 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,6 +15,11 @@ forums](https://discuss.streamlit.io).
 In the meantime, below is an example of what you can do with just a few lines of code:
 """
 
+values = st.slider(
+    'Select a range of values',
+    0.0, 8.0, (25.0, 75.0))
+st.write('Values:', values)
+
 
 with st.echo(code_location='below'):
     total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
